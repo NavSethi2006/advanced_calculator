881bb5a372b2665fc2a79bce7c4d129a0c20808b
NavSethi2006 <navinsethi2006@gmail.com>
2023-10-17 18:14:49 -0400

    gtk now working

--- Makefile
+++ Makefile
@@ -1,12 +1,12 @@
 CXX		  := gcc
-CXX_FLAGS := -Wall -Wextra `pkg-config --cflags glib-2.0`
+CXX_FLAGS := -Wall -Wextra -std=c17 -ggdb
 
 BIN		:= bin
 SRC		:= src
-INCLUDE	:= include/gtk-4.0
+INCLUDE	:= include
 LIB		:= lib
 
-LIBRARIES	:= `pkg-config --libs glib-2.0` -pthread -lgtk-4
+LIBRARIES	:= `pkg-config --cflags --libs gtk+-3.0`
 EXECUTABLE	:= main
 
 
--- src/main.c
+++ src/main.c
@@ -1,10 +1,40 @@
+/**
+ * @file main.c
+ * 
+ * @author your name (@TigerNav)
+ * @brief Inits GTK and all of its stuff. I dont like a lot of stuff in my main method so yea
+ * 
+ * @version 0.1
+ * @date 2022-02-05
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
 #include <stdio.h>
-#include <gtk/gtk.h>
+#include <gtk-3.0/gtk/gtk.h>
 
-int main() {
 
-    printf("hello world\n");
+static void activate (GtkApplication* app, gpointer user_data)
+{
+  GtkWidget *window;
 
-    return 0;
-
+  window = gtk_application_window_new (app);
+  gtk_window_set_title (GTK_WINDOW (window), "Window");
+  gtk_window_set_default_size (GTK_WINDOW (window), 200, 200);
+  gtk_widget_show_all (window);
 }
+
+int main (int    argc, char **argv){
+  GtkApplication *app;
+  int status;
+
+  app = gtk_application_new ("org.gtk.example", G_APPLICATION_FLAGS_NONE);
+  g_signal_connect (app, "activate", G_CALLBACK (activate), NULL);
+  status = g_application_run (G_APPLICATION (app), argc, argv);
+  g_object_unref (app);
+
+  return status;
+}
+
+
